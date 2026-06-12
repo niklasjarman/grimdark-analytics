435cc40fee90c938e03b32cc0baa72641a84d3c5
niklasjarman <njarma55@gmail.com>
2026-06-11 22:55:24 -0400

    initial Spring Boot scaffold

---
+++ grimdark-analytics/src/main/java/com/grimdark_analytics/grimdark_analytics/GrimdarkAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.grimdark_analytics.grimdark_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimdarkAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimdarkAnalyticsApplication.class, args);
+	}
+
+}
---
+++ grimdark-analytics/src/test/java/com/grimdark_analytics/grimdark_analytics/GrimdarkAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grimdark_analytics.grimdark_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimdarkAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
